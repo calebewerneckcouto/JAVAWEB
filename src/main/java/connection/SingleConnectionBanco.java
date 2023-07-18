64c4913ba947d55aa5dc7d783574ad08a5f1dbee
CHIP7 <CHIP7@DESKTOP-995CF5N>
2023-07-18 13:10:47 -0300

    atualizando

--- src/main/java/connection/SingleConnectionBanco.java
+++ src/main/java/connection/SingleConnectionBanco.java
@@ -4,16 +4,20 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class SingleConnectionBanco {
-	/*
+	
 	private static String banco = "jdbc:postgresql://localhost:5432/cwcdcomb_curso-jsp?autoReconnect=true";
 	private static String user = "cwcdcomb";
 	private static String senha = "cwc3d14694899";
 	private static Connection connection = null;
-	*/
+	
+	
+	/*
 	private static String banco = "jdbc:postgresql://localhost:5432/curso-jsp?autoReconnect=true";
 	private static String user = "postgres";
 	private static String senha = "admin";
 	private static Connection connection = null;
+	
+	*/
 	
 	
 	public static Connection getConnection() {
